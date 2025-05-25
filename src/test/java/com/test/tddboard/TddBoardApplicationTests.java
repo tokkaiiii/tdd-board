36607356874f547a9c135bc9a05ee5b7cd96ad9e
heosangbeom <luv_3_u@naver.com>
2025-05-25 21:52:15 +0900

    first commit

---
+++ src/test/java/com/test/tddboard/TddBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.tddboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddBoardApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
