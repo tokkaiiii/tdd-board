7e835519e92903954dd83dc204c65edbfe8aa510
heosangbeom <luv_3_u@naver.com>
2025-05-26 17:01:48 +0900

    feat: 이메일 검증 및 테스트 추가

--- src/main/java/com/test/tddboard/api/controller/WriterSignupController.java
+++ src/main/java/com/test/tddboard/api/controller/WriterSignupController.java
@@ -1,14 +1,25 @@
 package com.test.tddboard.api.controller;
 
+import com.test.tddboard.command.CreateUserCommand;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WriterSignupController {
 
   @PostMapping("/writer/sign-up")
-  public ResponseEntity<?> signUp() {
+  public ResponseEntity<?> signUp(@RequestBody CreateUserCommand command) {
+    String emailRegex = "^[a-zA-Z0-9](?!.*\\.\\.)[a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+    if (command.email() == null) {
+      return ResponseEntity.badRequest().build();
+    }else if (command.email().contains("@") == false) {
+      return ResponseEntity.badRequest().build();
+    } else if (command.email().matches(emailRegex) == false) {
+      return ResponseEntity.badRequest().build();
+    }
     return ResponseEntity.noContent().build();
   }
 
--- src/test/java/com/test/tddboard/api/writer/signup/POST_specs.java
+++ src/test/java/com/test/tddboard/api/writer/signup/POST_specs.java
@@ -6,6 +6,8 @@ import com.test.tddboard.TddBoardApplication;
 import com.test.tddboard.command.CreateUserCommand;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
@@ -37,5 +39,56 @@ class POST_specs {
 
     // Assert
     assertThat(response.getStatusCode().value()).isEqualTo(204);
+  }
+  
+  @DisplayName("email 속성이 지정되지 않으면 400 Bad Request 상태코드를 반환한다")
+  @Test
+  void email_속성이_지정되지_않으면_400_Bad_Request_상태코드를_반환한다(
+      @Autowired TestRestTemplate client
+  ){
+    // Arrange
+    var command = new CreateUserCommand(
+        null,
+        "user1",
+        "password"
+    );
+    
+    // Act
+    ResponseEntity<Void> response = client.postForEntity("/writer/sign-up",
+        command,
+        Void.class);
+
+    // Assert
+    assertThat(response.getStatusCode().value()).isEqualTo(400);
+  }
+  
+  @DisplayName("email 속성이 올바른 형식을 따르지 않으면 400 Bad Request 상태코드를 반환한다")
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "invalid-email",
+      "invalid-email@.com",
+      "invalid-email@test",
+      "invalid-email@test.",
+      "invalid-email@test..com",
+      "@example.com",
+  })
+  void email_속성이_올바른_형식을_따르지_않으면_400_Bad_Request_상태코드를_반환한다(
+      String email,
+      @Autowired TestRestTemplate client
+  ){
+    // Arrange
+    var command = new CreateUserCommand(
+        email,
+        "user1",
+        "password"
+    );
+    
+    // Act
+    ResponseEntity<Void> response = client.postForEntity("/writer/sign-up",
+        command,
+        Void.class);
+    
+    // Assert
+    assertThat(response.getStatusCode().value()).isEqualTo(400);
   }
 }
