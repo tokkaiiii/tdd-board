7654b62316a149b0de535aaa95468737a311bb31
heosangbeom <luv_3_u@naver.com>
2025-05-26 22:19:15 +0900

    feat: 작성자 회원가입 컨트롤 작성 및 테스트 추가

--- src/main/java/com/test/tddboard/api/controller/WriterSignupController.java
+++ src/main/java/com/test/tddboard/api/controller/WriterSignupController.java
@@ -9,18 +9,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WriterSignupController {
 
+  public static final String EMAIL_REGEX = "^[a-zA-Z0-9](?!.*\\.\\.)[a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
   @PostMapping("/writer/sign-up")
   public ResponseEntity<?> signUp(@RequestBody CreateUserCommand command) {
-    String emailRegex = "^[a-zA-Z0-9](?!.*\\.\\.)[a-zA-Z0-9._%+-]*@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
 
-    if (command.email() == null) {
-      return ResponseEntity.badRequest().build();
-    }else if (command.email().contains("@") == false) {
-      return ResponseEntity.badRequest().build();
-    } else if (command.email().matches(emailRegex) == false) {
-      return ResponseEntity.badRequest().build();
+    if (isCommandValid(command)) {
+      return ResponseEntity.noContent().build();
     }
-    return ResponseEntity.noContent().build();
+    return ResponseEntity.badRequest().build();
+
+  }
+
+  private static boolean isCommandValid(CreateUserCommand command) {
+    return isEmailValid(command.email())
+        && isUsernameValid(command.username())
+        && isPasswordValid(command.password());
+  }
+
+  private static boolean isPasswordValid(String password) {
+    return password != null
+        && password.trim().length() >= 8;
+  }
+
+  private static boolean isUsernameValid(String username) {
+    return username != null
+        && username.trim().length() >= 3;
+  }
+
+  private static boolean isEmailValid(String email) {
+    return email != null
+        && email.matches(EMAIL_REGEX);
   }
 
 }
--- src/test/java/com/test/tddboard/api/writer/signup/POST_specs.java
+++ src/test/java/com/test/tddboard/api/writer/signup/POST_specs.java
@@ -40,19 +40,19 @@ class POST_specs {
     // Assert
     assertThat(response.getStatusCode().value()).isEqualTo(204);
   }
-  
+
   @DisplayName("email 속성이 지정되지 않으면 400 Bad Request 상태코드를 반환한다")
   @Test
   void email_속성이_지정되지_않으면_400_Bad_Request_상태코드를_반환한다(
       @Autowired TestRestTemplate client
-  ){
+  ) {
     // Arrange
     var command = new CreateUserCommand(
         null,
         "user1",
         "password"
     );
-    
+
     // Act
     ResponseEntity<Void> response = client.postForEntity("/writer/sign-up",
         command,
@@ -61,7 +61,7 @@ class POST_specs {
     // Assert
     assertThat(response.getStatusCode().value()).isEqualTo(400);
   }
-  
+
   @DisplayName("email 속성이 올바른 형식을 따르지 않으면 400 Bad Request 상태코드를 반환한다")
   @ParameterizedTest
   @ValueSource(strings = {
@@ -75,19 +75,119 @@ class POST_specs {
   void email_속성이_올바른_형식을_따르지_않으면_400_Bad_Request_상태코드를_반환한다(
       String email,
       @Autowired TestRestTemplate client
-  ){
+  ) {
     // Arrange
     var command = new CreateUserCommand(
         email,
         "user1",
         "password"
     );
-    
+
     // Act
     ResponseEntity<Void> response = client.postForEntity("/writer/sign-up",
         command,
         Void.class);
-    
+
+    // Assert
+    assertThat(response.getStatusCode().value()).isEqualTo(400);
+  }
+
+  @DisplayName("username 속성이 지정되지 않으면 400 Bad Request 상태코드를 반환한다")
+  @Test
+  void username_속성이_지정되지_않으면_400_Bad_Request_상태코드를_반환한다(
+      @Autowired TestRestTemplate client
+  ) {
+    // Arrange
+    var command = new CreateUserCommand(
+        "user@test.com",
+        null,
+        "password"
+    );
+
+    // Act
+    ResponseEntity<Void> response = client.postForEntity(
+        "/writer/sign-up",
+        command,
+        Void.class);
+
+    // Assert
+    assertThat(response.getStatusCode().value()).isEqualTo(400);
+  }
+
+  @DisplayName("username 속성이 올바른 형식을 따르지 않으면 400 Bad Request 상태코드를 반환한다")
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "",
+      "us",
+      "us ",
+  })
+  void username_속성이_올바른_형식을_따르지_않으면_400_Bad_Request_상태코드를_반환한다(
+      String username,
+      @Autowired TestRestTemplate client
+  ) {
+    // Arrange
+    var command = new CreateUserCommand(
+        "user@test.com",
+        username,
+        "password"
+    );
+
+    // Act
+    ResponseEntity<Void> response = client.postForEntity(
+        "/writer/sign-up",
+        command,
+        Void.class);
+
+    // Assert
+    assertThat(response.getStatusCode().value()).isEqualTo(400);
+  }
+
+  @DisplayName("password 속성이 지정되지 않으면 400 Bad Request 상태코드를 반환한다")
+  @Test
+  void password_속성이_지정되지_않으면_400_Bad_Request_상태코드를_반환한다(
+      @Autowired TestRestTemplate client
+  ) {
+    // Arrange
+    var command = new CreateUserCommand(
+        "user@test.com",
+        "user",
+        null);
+
+    // Act
+    ResponseEntity<Void> response = client.postForEntity(
+        "/writer/sign-up",
+        command,
+        Void.class);
+
+    // Assert
+    assertThat(response.getStatusCode().value()).isEqualTo(400);
+  }
+
+  @DisplayName("password 속성이 올바른 형식을 따르지 않으면 400 Bad Request 상태코드를 반환한다")
+  @ParameterizedTest
+  @ValueSource(strings = {
+      "",
+      "pass",
+      "1234567",
+      "1234567 ",
+  })
+  void password_속성이_올바른_형식을_따르지_않으면_400_Bad_Request_상태코드를_반환한다(
+      String password,
+      @Autowired TestRestTemplate client
+  ) {
+    // Arrange
+    var command = new CreateUserCommand(
+        "user@test.com",
+        "user",
+        password
+    );
+
+    // Act
+    ResponseEntity<Void> response = client.postForEntity(
+        "/writer/sign-up",
+        command,
+        Void.class);
+
     // Assert
     assertThat(response.getStatusCode().value()).isEqualTo(400);
   }
